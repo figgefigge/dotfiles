c1ee689ebd72484f3d5cc3181e2a9454ec8e2767
Fredric Johansson <40358791+figgefigge@users.noreply.github.com>
2026-09-15 13:29:51 +0200

    Strukturerat in i roles mm.

--- scripts/tpm2.sh
+++ scripts/tpm2.sh
@@ -1,10 +1,10 @@
 
 
-sudo dnf install clevis clevis-luks clevis-dracut clevis-udisks2 clevis-systemd
-sudo dracut -fv --regenerate-all
-sudo systemctl reboot
+# sudo dnf install clevis clevis-luks clevis-dracut clevis-udisks2 clevis-systemd
+# sudo dracut -fv --regenerate-all
+# sudo systemctl reboot
 
-sudo clevis luks bind -d /dev/nvme... tpm2 '{"pcr_ids":"1,4,5,7,9"}'
+# sudo clevis luks bind -d /dev/nvme... tpm2 '{"pcr_ids":"1,4,5,7,9"}'
 
 
 
